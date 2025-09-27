3f1ffb8cd7f0bf842b705ec18d8f1e1b054413a7
salmansdr <salmansdr@gmail.com>
2025-09-27 05:04:49 +0000

    project.cs changes

--- Server/ConstructionAPI/Controllers/ProjectsController.cs
+++ Server/ConstructionAPI/Controllers/ProjectsController.cs
@@ -6,11 +6,10 @@ namespace ConstructionAPI.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-
     public class ProjectsController : ControllerBase
     {
-    private readonly string _jsonPath = "../Server/projects.json";
-
+    //private readonly string _jsonPath = "../Server/projects.json";
+    private readonly string _jsonPath = Path.Combine(Directory.GetCurrentDirectory(), "projects.json");
         [HttpGet]
         public async Task<IActionResult> GetProjects()
         {
--- Server/ConstructionAPI/Program.cs
+++ Server/ConstructionAPI/Program.cs
@@ -30,3 +30,4 @@ app.UseCors("AllowReactApp");
 app.MapControllers();
 
 app.Run();
+
